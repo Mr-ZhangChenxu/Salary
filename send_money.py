62c1d7fe1928c30719e47095c643ff12f8d7b205
Mr-ZhangChenxu <77765807+Mr-ZhangChenxu@users.noreply.github.com>
2021-01-24 17:10:29 +0800

    Update send_money.py

--- send_money.py
+++ send_money.py
@@ -6,14 +6,14 @@
 3、定义工资查询模块 select_money，用来展示工资数额
 4、定义一个start.py ，启动文件展示最终存款金额
 '''
-from money import saved_money
-
+# from money import saved_money
+import money
 
 def send_money(now_money=0):
-    now_money += saved_money
+    now_money += money.saved_money
     return now_money
 
 if __name__ == '__main__':
-    print(f'原有工资为：{saved_money}')
+    print(f'原有工资为：{money.saved_money}')
     now_money = send_money(now_money=1000)
     print(f'现有工资为：{now_money}')
